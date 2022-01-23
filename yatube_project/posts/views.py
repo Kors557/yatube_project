6b6af8f1c6f69031ee10342daecfdfd94e72ddde
Kors557 <1500459@mail.ru>
2022-01-23 22:04:12 +0300

    Не уверен что всё правильно, но пока работает

--- yatube_project/posts/views.py
+++ yatube_project/posts/views.py
@@ -1,24 +1,32 @@
-from re import template
-from turtle import title
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
     template = 'posts/index.html'
     title = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': title
+        'title': title,
+        'posts': posts
     }
     return render(request, template, context)
+
 
 def group_list(request):
     template = 'posts/group_list.html'
     title = 'Здесь будет информация о группах проекта Yatube'
     context = {
-        'title' : title
+        'title': title
     }
     return render(request, template, context)
-  
+
+
 def group_posts(request, slug):
-    return HttpResponse(f'Страницы сообществ {slug}')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
